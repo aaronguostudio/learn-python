fd5e11b24cbc133fb83e80cd55507c932a22d4f8
aaronguostudio <aaron.guo@payload.com>
2019-07-17 07:19:45 -0600

    add prime number calculator

--- python-100-days/day-3-4-branches-loop/is_prime.py
+++ python-100-days/day-3-4-branches-loop/is_prime.py
@@ -1,2 +1,16 @@
-# starts from here
-https://github.com/aaronguostudio/Python-100-Days/blob/master/Day01-15/04.%E5%BE%AA%E7%8E%AF%E7%BB%93%E6%9E%84.md
+# 素数，又称质数 prime number
+from math import sqrt
+
+num = int(input("请输入一个整数："))
+end = int(sqrt(num))
+
+is_prime = True
+
+for i in range(2, end + 1):
+  if num % i == 0:
+    is_prime = False
+    break
+if is_prime and num != 1:
+  print("%d is prime" % (num))
+else:
+  print("%d is not prime" % (num))
